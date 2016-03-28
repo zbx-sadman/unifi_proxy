11002a55e292dd5fc266e26b006f6a4b6199cbcd
zbx-sadman <sadman41@ya.ru>
2016-03-28 11:42:31 +0300

    .

--- src/modules/load.sh
+++
@@ -1,9 +0,0 @@
-make
-#exit
-chown zabbix:zabbix unifi.so
-chmod 644 unifi.so
-
-mkdir /usr/local/lib/zabbix
-mv -f ./unifi.so /usr/local/lib/zabbix/
-service zabbix-agent restart
-ps ax | grep zabbix_agentd
